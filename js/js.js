0bed81c035c20b2b2217dca754794ca62ab37295
Deepak Gautam <18037139+Deepak5j@users.noreply.github.com>
2020-03-12 01:26:42 +0530

    Update js.js
    updating js.js and adding functionality

--- js/js.js
+++ js/js.js
@@ -1,1 +1,66 @@
-alert(" :D ");
+function img_create(src, alt, title) {
+    //var img = IEWIN ? new Image() : document.createElement('img');
+    var img = new Image();
+    img.src = src;
+    img.SameSite = "None";
+    if ( alt != null ) img.alt = alt;
+    if ( title != null ) img.title = title;
+    return img;
+}
+
+function cleanUp() {
+    document.getElementById("but").disabled = false;
+    document.getElementById("thumbnails").innerHTML = "";  
+}
+
+function app() {
+    document.getElementById("but").disabled = true;
+    var url = document.getElementById("videoId").value;
+    var vid = "";
+    var reg = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    var match = url.match(reg);
+    if ( match && match[7].length == 11 ){
+        vid = "https://img.youtube.com/vi/" + match[7];
+    } else{
+        alert("Could not extract video ID. Possibly wrong url");
+        return;
+    }
+    
+    imgUrl1 = vid + "/1.jpg";
+    imgUrl2 = vid +"/2.jpg"
+    imgUrl3 = vid +"/3.jpg"
+    imgUrlDefault = vid +"/default.jpg"
+    imgUrlMqDefault = vid + "/mqdefault.jpg"
+    imgUrlHqDefault = vid + "/hqdefault.jpg"
+    imgUrlMaxResDefault = vid +"/maxresdefault.jpg"
+
+    var img1 = img_create(imgUrl1, "auto thumb-1", "Thumbnail1")
+    var img2 = img_create(imgUrl2, "auto thumb-2", "Thumbnail2")
+    var img3 = img_create(imgUrl3, "auto thumb-3", "Thumbnail3")
+    var imgDefault = img_create(imgUrlDefault, "Default", "Default")
+    var imgMqDefault = img_create(imgUrlMqDefault, "MQ Default", "MQ Default")
+    var imgHqDefault = img_create(imgUrlHqDefault, "HQ Default", "HQ Default")
+    var imgMaxResDefault = img_create(imgUrlMaxResDefault, "Maxres Default", "Maxres Default")
+   
+    var imgUrlsElements = [imgMaxResDefault, imgHqDefault, imgMqDefault, imgDefault, img3, img2, img1]; 
+
+    for(var i=0; i<imgUrlsElements.length; i++) {
+        var imgContainer = document.createElement("div");
+        imgContainer.id = "imgContainer";
+
+        var link = document.createElement("a");
+        link.href = imgUrlsElements[i].src;
+        link.download =  imgUrlsElements[i].title + ".jpg";
+        link.appendChild(imgUrlsElements[i]);
+
+        imgContainer.appendChild(link);
+
+        var titleContainer = document.createElement("div");
+        titleContainer.id = "titleContainer";
+        titleContainer.innerHTML = "<p>" + imgUrlsElements[i].title+ "</p>"
+        imgContainer.appendChild(titleContainer);        
+    
+        document.getElementById("thumbnails").appendChild(imgContainer);  
+    }
+    
+}
